1d5865c3461d0402f26813fd456761825ac7f4b9
akshay-jadhav-cis <aj567577@gmail.com>
2026-07-22 00:27:26 +0530

    first commit

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -2,7 +2,6 @@ package main
 
 import (
 	"fmt"
-	"strings"
 	"time"
 
 	"github.com/akshay-jadhav-cis/broadband-speed-tester/internal/routes"
@@ -13,16 +12,29 @@ import (
 func main() {
 	fmt.Println("Hello ")
 	router := gin.Default()
+	router.Use(func(c *gin.Context) {
+		fmt.Printf("Method=%s Origin=%q\n",
+			c.Request.Method,
+			c.GetHeader("Origin"),
+		)
+		c.Next()
+	})
 	config := cors.Config{
-		AllowOriginFunc: func(origin string) bool {
-        return origin == "http://localhost:5000" ||
-               strings.HasSuffix(origin, ".vercel.app")
-    },
-		AllowMethods:     []string{"GET", "PUT", "POST", "DELETE", "OPTIONS"},
-		AllowHeaders:     []string{"Origin", "Content-Type", "Accept", "Authorization"},
+		AllowOrigins: []string{
+			"https://speed-tester-wheat.vercel.app",
+			"http://localhost:5000",
+		},
+		AllowMethods: []string{
+			"GET", "POST", "PUT", "DELETE", "OPTIONS",
+		},
+		AllowHeaders: []string{
+			"Origin", "Content-Type", "Accept", "Authorization",
+		},
 		AllowCredentials: true,
 		MaxAge:           12 * time.Hour,
 	}
+
+	router.Use(cors.New(config))
 	router.Use(cors.New(config))
 	router.SetTrustedProxies([]string{"127.0.0.1"})
 	routes.AllRoutes(router)
